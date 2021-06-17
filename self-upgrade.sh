db34b947a2676d5506c7e6ba5e0b52392f105cd9
chenchuk77 <chenchuk@gmail.com>
2021-06-18 01:20:05 +0300

    rename

--- self-upgrade.sh
+++ self-upgrade.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "not ready"
+exit 0
+
+
+
 ./self-upgrade.sh {source-bucket} {service-name}
 
 
@@ -12,30 +17,45 @@ function log {
  # TODO: curl to elastic search for logging
 }
 
+function get_tomcat_home {
+ if [[ -z "${TENANT}" ]]; then
+   TOMCAT_HOME=/opt/lms/apache-tomcat-7
+ elif
+   TOMCAT_HOME=/opt/lms/apache-tomcat-7-${TENANT}
+ fi
+}
 
 
 TS=$(date +%s)
 
 log "source-ip:$(curl ifconfig.io) process starts with pid: $$, artifacts timestamp will be ${TS}"
 
+# from args
 S3_BUCKET=my_bucket
-SELFUPDATE_HOME=/opt/self-update
+WAR=
+TENANT=
 
-
+# consts
 # WORKSPACE is unique with TS and PID. this allows allinone to start multiple
 # processes in the background, each will have its own folder.
 WORKSPACE=${SELFUPDATE_HOME}/backups/${TS}-$$
+SELFUPDATE_HOME=/opt/self-update
+
+
 mkdir -p ${WORKSPACE}
 
-TOMCAT_HOME=/opt/lms/apache-tomcat-7
+get_tomcat_home
+log "using TOMCAT_HOME=${TOMCAT_HOME}."
 
-cp ${TOMCAT_HOME}/webapps/lms.war ${WORKSPACE}
+#backup_webapps
+cp ${TOMCAT_HOME}/webapps/${WAR}.war ${WORKSPACE}
 cp ${TOMCAT_HOME}/conf/Catalina/localhost/xxx.xml ${WORKSPACE}
 
+#delete_webapps
 rm -rf ${TOMCAT_HOME}/work/*
 rm -rf ${TOMCAT_HOME}/temp/*
-rm -rf ${TOMCAT_HOME}/webapps/lms
-rm -rf ${TOMCAT_HOME}/webapps/lms.war
+rm -rf ${TOMCAT_HOME}/webapps/${WAR}
+rm -rf ${TOMCAT_HOME}/webapps/${WAR}.war
 
 
 
